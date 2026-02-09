e3c046d332f0dd841975c0c1231451b62a200598
Williams Hernández <hernandw@gmail.com>
2026-02-09 19:08:27 -0300

    :sparkles: modificar  a traves de su id los elementos html

--- js/main.js
+++ js/main.js
@@ -1,1 +1,9 @@
 //accediendo a traves de su ID
+
+/* const titulo = document.getElementById('title')
+
+ titulo.innerHTML = "Soy el titulo modificado desde JS con getElementByID" */
+
+ const titulo = document. querySelector('#title')
+  titulo.innerHTML= "<strong>Soy el titulo <span id='modificado'>modificado</span> desde JS con query Selector</strong>"
+console.log(titulo)
